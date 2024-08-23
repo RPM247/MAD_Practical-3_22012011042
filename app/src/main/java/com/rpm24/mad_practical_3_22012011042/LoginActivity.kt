5b419e79fc9f7569ae506a1db4232d89d36108f0
PRIYANSHU RAMI <ramipriyanshu2004@gmail.com>
2024-08-23 10:43:52 +0530

    Initial commit

---
+++ app/src/main/java/com/rpm24/mad_practical_3_22012011042/LoginActivity.kt
@@ -0,0 +1,20 @@
+package com.rpm24.mad_practical_3_22012011042
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_login)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
---
+++ app/src/main/java/com/rpm24/mad_practical_3_22012011042/MainActivity.kt
@@ -0,0 +1,75 @@
+package com.rpm24.mad_practical_3_22012011042
+
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.provider.Telephony.Mms.Intents
+import android.widget.Button
+import android.widget.EditText
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val urlbtn = findViewById<Button>(R.id.urlbtn)
+        val urlinput = findViewById<EditText>(R.id.urlinput)
+        urlbtn.setOnClickListener{
+            val urlIntent = Intent(Intent.ACTION_VIEW)
+            urlIntent.setData(android.net.Uri.parse(urlinput.text.toString()))
+            startActivity(urlIntent)
+        }
+
+        val phonebtn = findViewById<Button>(R.id.phonebtn)
+        val phoneinput = findViewById<EditText>(R.id.phoneinput)
+        phonebtn.setOnClickListener {
+            val phoneNumber = phoneinput.text.toString()
+            val phoneIntent = Intent(Intent.ACTION_CALL).apply {
+                data = Uri.parse("tel:$phoneNumber")
+            }
+            startActivity(phoneIntent)
+        }
+
+        val clbtn = findViewById<Button>(R.id.clbtn)
+        clbtn.setOnClickListener{
+            val clIntent = Intent(Intent.ACTION_VIEW)
+            clIntent.setData(android.net.Uri.parse("content://call_log/calls"))
+            startActivity(clIntent)
+        }
+
+        val glbtn = findViewById<Button>(R.id.glbtn)
+        glbtn.setOnClickListener{
+            val glIntent = Intent(Intent.ACTION_VIEW)
+            glIntent.setData(android.net.Uri.parse("content://media/internal/images/media"))
+            startActivity(glIntent)
+        }
+
+        val cmbtn = findViewById<Button>(R.id.cmbtn)
+        cmbtn.setOnClickListener{
+            val cmIntent = Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE)
+            startActivity(cmIntent)
+        }
+
+        val albtn = findViewById<Button>(R.id.albtn)
+        albtn.setOnClickListener{
+            val alIntent = Intent(android.provider.AlarmClock.ACTION_SHOW_ALARMS)
+            startActivity(alIntent)
+        }
+
+        val logbtn = findViewById<Button>(R.id.lgbtn)
+        logbtn.setOnClickListener{
+            val logIntent = Intent(this,LoginActivity::class.java)
+            startActivity(logIntent)
+        }
+    }
+}
